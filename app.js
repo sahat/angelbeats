7bf0aa6b029fcc4abc861cf137c884702d380399
Sahat Yalkabov <sakhat@gmail.com>
2014-02-08 17:00:58 -0500

    Added upload middleware

--- app.js
+++ app.js
@@ -59,6 +59,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.logger('dev'));
+app.use('/upload', upload.fileHandler());
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
