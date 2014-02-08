ec741c5df599fa692c1410d417afde5ba2f6cfbe
Sahat Yalkabov <sakhat@gmail.com>
2014-02-08 16:31:17 -0500

    Added on socket connect login that handles host vs guest mode

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -19,4 +19,36 @@ $(document).ready(function() {
   $('#total-time').text(time.hours() + hours + time.minutes() + minutes);
 
 
+  socket.on('connect', function() {
+    if (localStorage.getItem('isHost')) {
+      socket.emit('hostComeback', this.socket.sessionid);
+      socket.emit('checkIfHost', this.socket.sessionid);
+    }
+
+
+    socket.emit('checkIfHost', this.socket.sessionid);
+    socket.on('checkIfHostAnswer', function(isHost) {
+      if (!isHost) {
+        console.log('You are not a host');
+        $('#play').unbind('click');
+        $('.track').unbind('dblclick');
+        $('#pause').unbind('click');
+        $('#forward').unbind('click');
+        $('#backward').unbind('click');
+        $('.guest-mode').show();
+      } else {
+        console.log('Yes you are a host');
+        $('.host-mode').show();
+        localStorage.setItem('isHost', true);
+
+        // If host, set localstorage variable isHost = true
+        // When disconnected, check on server if host is disconnected.
+        // If it is, create a 60 sec timeout, after which hostId is next socket in line
+        // If host comes back within 60 sec, clear the timeout
+
+      }
+    });
+
+  });
+
 });
