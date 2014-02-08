0c4304fbe544200d4b634a67a9e5753ff826ec24
Sahat Yalkabov <sakhat@gmail.com>
2014-02-08 16:29:58 -0500

    Update song count and total track play time

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,4 +2,21 @@ var socket = io.connect(window.location.href);
 
 $(document).ready(function() {
 
+  $('#song-count').text($('.track').length + ' tracks')
+
+  // Get total time
+  var total = 0;
+  $('.time').each(function(index) {
+    total += $(this).data('time');
+  });
+  var time = moment.duration({s: total });
+  var hours = ' hours ';
+  var minutes = ' minutes';
+
+  if (time.hours() === 1) hours = ' hour ';
+  if (time.minutes() === 1) minutes = ' minute';
+
+  $('#total-time').text(time.hours() + hours + time.minutes() + minutes);
+
+
 });
